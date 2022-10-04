c4a5ec185b46b73e14c2855ba8d56328ce1e04b4
Pranshu1902 <pranshu1902@gmail.com>
2022-10-04 18:20:11 +0530

    added styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ import image from "../assets/404.jpg";
 
 export default function NotFound() {
   return (
-    <div>
+    <div style={"display: flex; justify-content: center; align-items: center;"}>
       <img src={image} width={"70%"} height={"70%"} />
     </div>
   );
