7f0f8371305848fcace7bd29753a12de8086dd79
jmmendivil <jmanuel84@gmail.com>
2022-10-05 10:29:50 +0000

    add language switch button (#58)
    Co-authored-by: Andre Gabriel <andrgab@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,24 +10,22 @@ import "./i18n";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <UserProvider>
-      <ThemeProvider>
-        <CssBaseline />
-        <App />
-        <ToastContainer
-          autoClose={5000}
-          hideProgressBar={false}
-          position="bottom-center"
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-        />
-      </ThemeProvider>
-    </UserProvider>
-  </React.StrictMode>,
+  <UserProvider>
+    <ThemeProvider>
+      <CssBaseline />
+      <App />
+      <ToastContainer
+        autoClose={5000}
+        hideProgressBar={false}
+        position="bottom-center"
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+      />
+    </ThemeProvider>
+  </UserProvider>,
   document.getElementById("root")
 );
 reportWebVitals();
