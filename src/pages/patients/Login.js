e31d1e18788d754cddb9a68710bbce4f3553817d
Casey Ocampo <caseysoflo@gmail.com>
2022-10-06 18:24:53 -0400

    Add semantic heading to Login page

--- src/pages/patients/Login.js
+++ src/pages/patients/Login.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, withRouter } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import { useUserDispatch, loginUser } from "../../context/UserContext";
-import api from "../../services/Api";
-import { toast } from "react-toastify";
-import AppBarMediRepo from "../components/AppBarMediRepo";
-import { useTranslation } from "react-i18next";
+import React, { useState, useEffect } from "react"
+import { useHistory, withRouter } from "react-router-dom"
+import Button from "@material-ui/core/Button"
+import LinearProgress from "@material-ui/core/LinearProgress"
+import TextField from "@material-ui/core/TextField"
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
+import Select from "@material-ui/core/Select"
+import Link from "@material-ui/core/Link"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
+import OutlinedInput from "@material-ui/core/OutlinedInput"
+import { useUserDispatch, loginUser } from "../../context/UserContext"
+import api from "../../services/Api"
+import { toast } from "react-toastify"
+import AppBarMediRepo from "../components/AppBarMediRepo"
+import { useTranslation } from "react-i18next"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -50,58 +50,58 @@ const useStyles = makeStyles((theme) => ({
   progress: {
     marginTop: theme.spacing(1),
   },
-}));
+}))
 
 function SignIn() {
-  var userDispatch = useUserDispatch();
+  var userDispatch = useUserDispatch()
 
-  const classes = useStyles();
-  var [isLoading, setIsLoading] = useState(false);
-  var [login, setCodpac] = useState("");
-  var [password, setPassword] = useState("");
-  var [dt_nasc, setDtnasc] = useState("");
-  var [id, setHospital] = useState("");
-  var [listHosps, setlistHosp] = useState([]);
-  const { t } = useTranslation();
-  const notify = () => toast.info(t("askPatientLogin"));
+  const classes = useStyles()
+  var [isLoading, setIsLoading] = useState(false)
+  var [login, setCodpac] = useState("")
+  var [password, setPassword] = useState("")
+  var [dt_nasc, setDtnasc] = useState("")
+  var [id, setHospital] = useState("")
+  var [listHosps, setlistHosp] = useState([])
+  const { t } = useTranslation()
+  const notify = () => toast.info(t("askPatientLogin"))
 
-  const history = useHistory();
+  const history = useHistory()
 
   useEffect(() => {
     api
       .get("/hospitals/list")
-      .then((response) => {
-        setlistHosp(response.data.hospital);
+      .then(response => {
+        setlistHosp(response.data.hospital)
       })
-      .catch((err) => {
-        console.log(err);
-        toast.error(t("loadHospitalsError"));
-      });
-  }, [t]);
+      .catch(err => {
+        console.log(err)
+        toast.error(t("loadHospitalsError"))
+      })
+  }, [t])
 
   async function handleLogin(e) {
-    e.preventDefault();
+    e.preventDefault()
 
     const data = {
       login,
       password,
       dt_nasc,
       id,
-    };
+    }
 
     if (!!login && !!password && !!dt_nasc && !!id) {
-      setIsLoading(true);
+      setIsLoading(true)
       try {
-        const response = await api.post("patients/signin", data);
-        var token = response.data.token;
+        const response = await api.post("patients/signin", data)
+        var token = response.data.token
 
-        loginUser(userDispatch, token);
-        setIsLoading(false);
-        history.push("/patients/bulletin");
+        loginUser(userDispatch, token)
+        setIsLoading(false)
+        history.push("/patients/bulletin")
       } catch (err) {
-        console.log(err);
-        setIsLoading(false);
-        toast.dark(t("authenticationRequired"));
+        console.log(err)
+        setIsLoading(false)
+        toast.dark(t("authenticationRequired"))
       }
     }
   }
@@ -113,51 +113,13 @@ function SignIn() {
       </div>{" "}
       <Container component="main" maxWidth="xs">
         <div className={classes.paper}>
-          <Typography color="primary" component="h6" variant="button">
+          <Typography color="primary" component="h1" variant="button">
             PATIENT LOGIN
           </Typography>
           <form className={classes.form} noValidate onSubmit={handleLogin}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="cod_pac"
-              label={t("patientCode")}
-              name="cod_pac"
-              autoFocus
-              onChange={(e) => setCodpac(e.target.value)}
-            />{" "}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="senha"
-              label={t("password")}
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />{" "}
-            <TextField
-              InputLabelProps={{ shrink: true }}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="data_nasc"
-              label={t("birthday")}
-              type="date"
-              id="data_nasc"
-              autoComplete={t("birthday")}
-              onChange={(e) => setDtnasc(e.target.value)}
-            />
+            <TextField variant="outlined" margin="normal" required fullWidth id="cod_pac" label={t("patientCode")} name="cod_pac" autoFocus onChange={e => setCodpac(e.target.value)} /> <TextField variant="outlined" margin="normal" required fullWidth name="senha" label={t("password")} type="password" id="password" autoComplete="current-password" onChange={e => setPassword(e.target.value)} /> <TextField InputLabelProps={{ shrink: true }} variant="outlined" margin="normal" required fullWidth name="data_nasc" label={t("birthday")} type="date" id="data_nasc" autoComplete={t("birthday")} onChange={e => setDtnasc(e.target.value)} />
             <FormControl className={classes.formControl} variant="outlined">
-              <InputLabel
-                htmlFor="nascimento"
-                id="simple-select-outlined-label"
-              >
+              <InputLabel htmlFor="nascimento" id="simple-select-outlined-label">
                 Hospital
               </InputLabel>
               <Select
@@ -169,18 +131,11 @@ function SignIn() {
                   name: "hospital",
                   id: "hospital_id",
                 }}
-                input={
-                  <OutlinedInput
-                    shrink="true"
-                    labelWidth={62}
-                    name="hospital"
-                    id="simple-select-outline"
-                  />
-                }
-                onChange={(e) => setHospital(e.target.value)}
+                input={<OutlinedInput shrink="true" labelWidth={62} name="hospital" id="simple-select-outline" />}
+                onChange={e => setHospital(e.target.value)}
               >
                 <option aria-label="None" value="" />
-                {listHosps.map((listHosp) => (
+                {listHosps.map(listHosp => (
                   <option value={listHosp.id}> {listHosp.name} </option>
                 ))}
               </Select>
@@ -188,13 +143,7 @@ function SignIn() {
             {isLoading ? (
               <LinearProgress className={classes.progress} />
             ) : (
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-              >
+              <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                 {t("login")}
               </Button>
             )}{" "}
@@ -214,6 +163,6 @@ function SignIn() {
         </div>
       </Container>{" "}
     </div>
-  );
+  )
 }
-export default withRouter(SignIn);
+export default withRouter(SignIn)
