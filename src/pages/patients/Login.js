f3fd85b43b626b29d6a9b8a9216d7a5538f9e405
jayesh-choudhary <jayesh.c@deuexsolutions.com>
2022-10-23 00:27:02 +0530

    Notify user to enter required inputs in login form

--- src/pages/patients/Login.js
+++ src/pages/patients/Login.js
@@ -17,6 +17,8 @@ import { toast } from 'react-toastify';
 import AppBarMediRepo from '../components/AppBarMediRepo';
 import { useTranslation } from 'react-i18next';
 import RouterLink from '../../components/RouterLink/RouterLink';
+import { Alert } from '@material-ui/lab';
+import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -64,6 +66,7 @@ function SignIn() {
   var [listHosps, setlistHosp] = useState([]);
   const { t } = useTranslation();
   const notify = () => toast.info(t('askPatientLogin'));
+  const [isFormInComplete, setIsFormInComplete] = useState(false);
 
   const history = useHistory();
 
@@ -90,6 +93,7 @@ function SignIn() {
     };
 
     if (!!login && !!password && !!dt_nasc && !!id) {
+      setIsFormInComplete(false);
       setIsLoading(true);
       try {
         const response = await api.post('patients/signin', data);
@@ -103,6 +107,8 @@ function SignIn() {
         setIsLoading(false);
         toast.dark(t('authenticationRequired'));
       }
+    } else {
+      setIsFormInComplete(true);
     }
   }
 
@@ -116,6 +122,13 @@ function SignIn() {
           <Typography color="primary" component="h1" variant="button">
             PATIENT LOGIN
           </Typography>
+
+          {isFormInComplete && (
+            <Box width="100%" mt={3}>
+              <Alert severity="error">{t('fillRequiredFields')}</Alert>
+            </Box>
+          )}
+
           <form className={classes.form} noValidate onSubmit={handleLogin}>
             <TextField
               variant="outlined"
