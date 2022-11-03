823e19dcba36d7517c3536feeed9cac2985a6ccd
LN <91385411+Ln11211@users.noreply.github.com>
2022-11-03 05:56:22 +0530

    [Docs] Added Sinhalese translation (#149)
    * Update i18n.js
    
    Added Sinhalese
    
    * Create si.json
    
    Add Sinhalese translations
    
    * Fixed a single typo
    
    There is a hindi "Ra" in line 19
    
    * Rename si.json to content.json
    
    * Update src/i18n.js
    
    Co-authored-by: Andre Gabriel <andrgab@gmail.com>

--- src/i18n.js
+++ src/i18n.js
@@ -42,6 +42,7 @@ import contentZhCN from './locales/zh_CN/content.json';
 import contentMa from './locales/ma/content.json';
 import contentVi from './locales/vi/content.json';
 import contentSl from './locales/sl/content.json';
+import contentSi from './locales/si/content.json';
 import contentDa from './locales/da/content.json';
 import contentAm from './locales/am/content.json';
 
@@ -97,6 +98,7 @@ const resources = {
   ma: contentMa,
   vi: contentVi,
   sl: contentSl,
+  si: contentSi,
   da: contentDa,
   am: contentAm,
 };
@@ -138,6 +140,8 @@ export const localesList = [
   { locale: 'pt-BR', description: 'Português' },
   { locale: 'ru', description: 'Russian' },
   { locale: 'sm', description: 'Samoan' },
+  { locale: 'sl', description: 'Slovenian' },
+  { locale: 'si', description: 'Sinhalese' },
   { locale: 'sv', description: 'Swedish' },
   { locale: 'ta', description: 'Tamil' },
   { locale: 'te', description: 'Telugu' },
@@ -145,7 +149,6 @@ export const localesList = [
   { locale: 'uk', description: 'Ukraine' },
   { locale: 'ur', description: 'Urdu' },
   { locale: 'vi', description: 'Vietnamese' },
-  { locale: 'sl', description: 'Slovenian' },
 ];
 
 i18n
